b5121a65ce2c7c67907b95176faf70ee3af7bd43
Carlholmstrom <54806294+Carlholmstrom@users.noreply.github.com>
2023-01-16 14:17:17 +0100

    Added blog posts

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/Footer.css";
+import { BsGithub } from "react-icons/bs";
+
+const Footer = () => {
+  return (
+    <footer className="footer-style">
+      <nav>
+        <p>View project on GitHub</p>
+        <br />
+        <a href="https://github.com/Carlholmstrom/tiny-blog-react" target="_blank">
+          <BsGithub color="white" size={40} />
+        </a>
+      </nav>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../styles/Header.css"
+import { BsGithub } from 'react-icons/bs'
+
+
+const Header = () => {
+    return (
+        <header className="header-style">
+            <h1 className="header-title">The Tiny Blog</h1>
+        </header>
+    );
+};
+
+export default Header;
